23b7dfba5f60009ec2c3b9ef976d7099d0d95e4b
Dmitriy Tuchashvili <pechorin211@gmail.com>
2024-10-18 17:38:04 +0500

    removed cyclic dependecies

--- dpg_sim_game/Scenes/Common/EventSystem/EventPredicateFactory.gd
+++ dpg_sim_game/Scenes/Common/EventSystem/EventPredicateFactory.gd
@@ -1,4 +1,5 @@
 class_name EventPredicateFactory
+extends Reference
 
 enum PredicateType {
     TIME,
@@ -16,31 +17,12 @@ enum PredicateType {
 }
 
 
-var _main_session: MainSession
-var _event_manager: EventManager
-var _team_screen: TeamScreen
-var _money_node: Money
-var _fit_points: PointCounter
-var _dev_points: PointCounter
-var _market_points: PointCounter
+var _game: MainGame
 
 
-func _init(
-    main_session: MainSession,
-    event_manager: EventManager, 
-    team_screen: TeamScreen, 
-    money_node: Money,
-    fit_points: PointCounter,
-    dev_points: PointCounter,
-    market_points: PointCounter):
+func _init(game: MainGame):
+    _game = game
 
-    _main_session = main_session
-    _event_manager = event_manager
-    _team_screen = team_screen
-    _money_node = money_node
-    _fit_points = fit_points
-    _dev_points = dev_points
-    _market_points = market_points
 
 
 ## Returns appropriate predicate (condition) given type and parameters
@@ -48,43 +30,43 @@ func create_predicate(predicate_type: int, parameter: String) -> EventPredicate:
     match predicate_type:
 
         PredicateType.TIME:
-            return TimePredicate.new(parameter.to_int(), _main_session)
+            return TimePredicate.new(parameter.to_int(), _game)
 
         PredicateType.ACTION:
             assert(false, "Not implemented")
             return null
 
         PredicateType.MONEY:
-            return MoneyPredicate.new(parameter.to_int(), _money_node)
+            return MoneyPredicate.new(parameter.to_int(), _game)
 
         PredicateType.FIT_POINTS:
-            return GoodPointsPredicate.new(parameter.to_int(), _fit_points)
+            return GoodPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/FitCounter"))
 
         PredicateType.INCONSISTENCY_POINTS:
-            return BadPointsPredicate.new(parameter.to_int(), _fit_points)
+            return BadPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/FitCounter"))
 
         PredicateType.DEV_POINTS:
-            return GoodPointsPredicate.new(parameter.to_int(), _dev_points)
+            return GoodPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/DevCounter"))
 
         PredicateType.TECH_DEPT_POINTS:
-            return BadPointsPredicate.new(parameter.to_int(), _dev_points)
+            return BadPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/DevCounter"))
 
         PredicateType.MARKET_POINTS:
-            return GoodPointsPredicate.new(parameter.to_int(), _market_points)
+            return GoodPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/MarketCounter"))
 
         PredicateType.REJECTION_POINTS:
-            return BadPointsPredicate.new(parameter.to_int(), _market_points)
+            return BadPointsPredicate.new(parameter.to_int(), _game.get_node("MainSession/MarketCounter"))
 
         PredicateType.TEAM_SIZE:
-            return TeamPredicate.new(parameter.to_int(), _team_screen)
+            return TeamPredicate.new(parameter.to_int(), _game)
 
         PredicateType.SESSION_STAGE:
-            return StagePredicate.new(parameter.to_int())
+            return StagePredicate.new(parameter.to_int(), _game)
 
         PredicateType.EVENT:
             var separator = "|"
             var params = parameter.split(separator)
-            return ParentEventPredicate.new(params[0].to_int(), params[1].to_int(), _event_manager, _main_session)
+            return ParentEventPredicate.new(params[0].to_int(), params[1].to_int(), _game)
 
         _:
             assert(false, "Unsupported predicate type")
@@ -95,6 +77,8 @@ func create_predicate(predicate_type: int, parameter: String) -> EventPredicate:
 
 ## Base class for all event predicates (conditions). 
 class EventPredicate:
+
+    extends Reference
 
     ## Checks if event condition criterias are met
     func evaluate() -> bool:
@@ -156,15 +140,16 @@ class TimePredicate:
 
     extends ValuePredicate
 
-    var _main_session: MainSession
+
+    var _game: MainGame
 
 
-    func _init(weeks: int, main_session: MainSession, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(weeks, comparison_type):
-        _main_session = main_session
+    func _init(weeks: int, game: MainGame, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(weeks, comparison_type):
+        _game = game
         
 
     func _get_current_value():
-        return int(_main_session.curDays / 7)
+        return int(_game.get_node("MainSession").curDays / 7)
         
 
 ## TODO: Implement this
@@ -179,15 +164,16 @@ class MoneyPredicate:
 
     extends ValuePredicate
 
-    var _moneyNode: Money
+
+    var _game: MainGame
 
 
-    func _init(target_money: int, moneyNode: Money, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_money, comparison_type):
-        _moneyNode = moneyNode
+    func _init(target_money: int, game: MainGame, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_money, comparison_type):
+        _game = game
 
     
     func _get_current_value():
-        return _moneyNode.total
+        return _game.get_node("Header/Money").total
 
 
 ## Checks for Good Points quantity
@@ -227,15 +213,16 @@ class TeamPredicate:
 
     extends ValuePredicate
 
-    var _team_screen: TeamScreen
+
+    var _game: MainGame
 
 
-    func _init(target_team_size: int, team_screen: TeamScreen, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_team_size, comparison_type):
-        _team_screen = team_screen
+    func _init(target_team_size: int, game: MainGame, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_team_size, comparison_type):
+        _game = game
 
 
     func _get_current_value():
-        return _team_screen.teamSize
+        return _game.get_node("TeamScreen").teamSize
 
 
 ## Checks for a particular session stage (phase)
@@ -244,12 +231,15 @@ class StagePredicate:
     extends ValuePredicate
 
 
-    func _init(target_stage: int, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_stage, comparison_type):
-        pass
+    var _game: MainGame
+
+
+    func _init(target_stage: int, game: MainGame, comparison_type: int = ComparisonType.GREATER_OR_EQUAL).(target_stage, comparison_type):
+        _game = game
 
 
     func _get_current_value():
-        return global.curPhaseIndex
+        return _game.global.curPhaseIndex
 
 
 ## A particular event was triggered
@@ -257,20 +247,18 @@ class ParentEventPredicate:
 
     extends EventPredicate
 
-    var _event_manager: EventManager
-    var _main_session: MainSession
     var _parent_event_id: int
     var _delay_weeks: int
+    var _game: MainGame
 
     var _is_parent_event_triggered: bool
     var _parent_event_triggered_day: int
 
 
-    func _init(parent_event_id: int, delay_weeks: int, event_manager: EventManager, main_session: MainSession):
+    func _init(parent_event_id: int, delay_weeks: int, game: MainGame):
         _parent_event_id = parent_event_id
         _delay_weeks = delay_weeks
-        _event_manager = event_manager
-        _main_session = main_session
+        _game = game
 
 
     func evaluate() -> bool:
@@ -282,10 +270,10 @@ class ParentEventPredicate:
 
 
     func check_parent_event():
-        if (_parent_event_id in _event_manager.completed_events):
-            _parent_event_triggered_day = _main_session.curDays
+        if (_parent_event_id in _game.get_node("EventManager").completed_events):
+            _parent_event_triggered_day = _game.get_node("MainSession").curDays
             _is_parent_event_triggered = true
 
     
     func check_delay() -> bool:
-        return (_main_session.curDays - _parent_event_triggered_day) >= _delay_weeks * 7
+        return (_game.get_node("MainSession").curDays - _parent_event_triggered_day) >= _delay_weeks * 7
--- dpg_sim_game/Scenes/Common/EventSystem/EventScheduler.gd
+++ dpg_sim_game/Scenes/Common/EventSystem/EventScheduler.gd
@@ -1,14 +1,12 @@
 class_name EventScheduler
+extends Reference
 
+                                    # TODO: Add to game settings
 var _slot_size: int = 7             # Size for each event slot. Values in range [1:7]
 var _event_queue: Array = []        # If several events should be triggered during one week, they get into the queue and be called on the next slot start
 var _event_pool: Array = []         # Pool of all available events for the scenario
 
-var _main_session: MainSession
-
-
-func _init(main_session: MainSession):
-    _main_session = main_session
+signal event_started(event)
 
 
 func populate_event_pool(events: Array):
@@ -19,18 +17,36 @@ func populate_event_pool(events: Array):
         _event_pool.append(event)
 
 
-func check_events():
+func check_day_for_events(day):
+    if (day % _slot_size == 0):
+        _start_event_slot()
+
+
+func reset():
+    _event_queue.clear()
+    _event_pool.clear()
+
+
+func _start_event_slot():
+    _check_events()
+
+    if (len(_event_queue) == 0):
+        return
+    var event = _event_queue.pop_front()
+    emit_signal("event_started", event)
+
+
+func _check_events():
     var remove_pool: Array = []
     for event in _event_pool:
-        if (!check_event_condition(event)):
+        if (!_check_event_condition(event)):
             continue
-        _event_queue.append(event)
+        _event_queue.push_back(event)
         remove_pool.append(event)
 
     for event in remove_pool:
         _event_pool.remove(event)
         
 
-
-func check_event_condition(event) -> bool:
+func _check_event_condition(event) -> bool:
     return event["Predicate"].evaluate()
